fd60205cc7d676594c6279c3a8950b7752b84731
diepsohung <diepsohung@gmail.com>
2018-07-19 16:19:07 +0700

    add say method to slack bot

--- lib/boot_slack_bot/api.rb
+++ lib/boot_slack_bot/api.rb
@@ -3,8 +3,12 @@ require 'httparty'
 module BootSlackBot
   extend self
 
-  def say(message)
-    HTTParty.post(BootSlackBot.config.slack_hook, body: message.to_json, headers: json_headers)
+  def say(message, web_hook = BootSlackBot.config.slack_hook)
+    if web_hook.blank?
+      raise StandardError, 'Web Hook URL is blank !'
+    else
+      HTTParty.post(web_hook, body: { text: message }.to_json, headers: json_headers)
+    end             
   end
 
   def json_headers
